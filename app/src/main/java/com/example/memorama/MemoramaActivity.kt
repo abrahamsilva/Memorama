7a595e4c70bacc03f6f29bdf7851f78240b2862a
Manuel Avalos Tovar <manuelavalos97@hotmail.es>
2019-03-15 23:38:27 -0600

    memorama implementado

--- app/src/main/java/com/example/memorama/MemoramaActivity.kt
+++ app/src/main/java/com/example/memorama/MemoramaActivity.kt
@@ -5,8 +5,12 @@ import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
 import com.example.games.R
 import kotlinx.android.synthetic.main.activity_memorama2.*
+import java.util.Random
+import java.util.HashMap
 
 class MemoramaActivity : AppCompatActivity() {
+
+    val random = Random()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,11 +20,50 @@ class MemoramaActivity : AppCompatActivity() {
         var grid = GridLayoutManager(this, 4)
         rv.layoutManager = grid
 
+        val keys:HashMap<Int, String> = HashMap()
+        val used:HashMap<String, Int> = HashMap()
+
+        used.put("arryn", 2)
+        used.put("baratheon", 2)
+        used.put("got", 2)
+        used.put("lannister", 2)
+        used.put("martell", 2)
+        used.put("stark", 2)
+        used.put("targaryen", 2)
+        used.put("tyrell", 2)
+
         val chips = ArrayList<Chip>()
-        for( i  in 0..12)
-            chips.add(Chip(R.mipmap.ic_launcher))
-        var  adapter = MemoramaAdapter(chips)
+
+        var random = 0
+
+        var arrayOfHouses = arrayOf(
+            "arryn",
+            "baratheon",
+            "got",
+            "lannister",
+            "martell",
+            "stark",
+            "targaryen",
+            "tyrell"
+        )
+
+        loop@ for( i  in 0..16){
+            random = rand(0, 8)
+
+            val aux = used[arrayOfHouses[random]]
+            if (aux == 0) continue@loop
+            if(aux != null) used.replace(arrayOfHouses[random], aux-1)
+            chips.add(Chip(R.mipmap.ic_launcher, i, arrayOfHouses[random]))
+            keys[i] = arrayOfHouses[random]
+        }
+
+
+        var  adapter = MemoramaAdapter(chips, keys)
         rv.adapter = adapter
 
+    }
+
+    fun rand(from: Int, to: Int) : Int { //from (inclusive) to (non-inclusive)
+        return random.nextInt(to - from) + from
     }
 }
--- app/src/main/java/com/example/memorama/MemoramaAdapter.kt
+++ app/src/main/java/com/example/memorama/MemoramaAdapter.kt
@@ -5,12 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
+import android.os.Handler
 import com.example.games.R
-import kotlinx.android.synthetic.main.renglon.view.*
+//import java.util.logging.Handler
 
-class MemoramaAdapter(val chips: ArrayList<Chip>):
+class MemoramaAdapter(val chips: ArrayList<Chip>, val keys: HashMap<Int,String>):
 
     RecyclerView.Adapter<MemoramaAdapter.ChipViewHolder>(){
+
+    var pushedButton: MutableList<Int> = mutableListOf()
+    var score = 0
+
+    var selected1: ImageView? = null
+    var selected2: ImageView? = null
+
+    var winScore = 8
+
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ChipViewHolder {
         val view = LayoutInflater.from(p0.context).inflate(R.layout.renglon, p0, false)
         return ChipViewHolder(view)
@@ -22,17 +33,73 @@ class MemoramaAdapter(val chips: ArrayList<Chip>):
 
     override fun onBindViewHolder(p0: ChipViewHolder, p1: Int) {
         p0.imageView.setImageResource(chips[p1].idImage)
+        p0.imageView.id = (chips[p1].pos)
     }
 
+    fun playIntent(item: ImageView){
+        if (selected1 == null) selected1 = item
+        else if (selected2 == null) selected2 = item
+    }
+
+    fun checkPair (item: View){
+        if (selected1 != null && selected2 != null){
+            if (keys[selected1!!.id] == keys[selected2!!.id]){
+                score++
+                println(score)
+                pushedButton.add(selected1!!.id)
+                pushedButton.add(selected2!!.id)
+                selected1 = null
+                selected2 = null
+                if(score==winScore)
+                    Toast.makeText(item.context, "Ganaste", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Handler().postDelayed({
+                    selected1!!.setImageResource(R.mipmap.ic_launcher)
+                    selected2!!.setImageResource(R.mipmap.ic_launcher)
+                    selected1 = null
+                    selected2 = null
+                }, 500)
+            }
+        }
+    }
 
     inner class ChipViewHolder(item : View) : RecyclerView.ViewHolder(item){
         val imageView = item.findViewById<ImageView>(R.id.chip)
 
         init {
             item.setOnClickListener {
-                imageView.setImageResource(R.drawable.rocket)
+                if(imageView.id !in pushedButton){
+                    when (keys[imageView.id]){
+                        "arryn" -> {
+                            imageView.setImageResource(R.drawable.arryn)
+                        }
+                        "baratheon" -> {
+                            imageView.setImageResource(R.drawable.baratheon)
+                        }
+                        "got" -> {
+                            imageView.setImageResource(R.drawable.got)
+                        }
+                        "lannister" -> {
+                            imageView.setImageResource(R.drawable.lannister)
+                        }
+                        "martell" -> {
+                            imageView.setImageResource(R.drawable.martell)
+                        }
+                        "stark" -> {
+                            imageView.setImageResource(R.drawable.stark)
+                        }
+                        "targaryen" -> {
+                            imageView.setImageResource(R.drawable.targaryen)
+                        }
+                        "tyrell" -> {
+                            imageView.setImageResource(R.drawable.tyrell)
+                        }
+                    }
+                    playIntent(imageView)
+                }
+                checkPair(item)
             }
         }
     }
-
 }
